3f78ec71c7c03a5f7eec9049029bad582dd35814
Renna Aprilia <rennaaprilia@student.uns.ac.id>
2023-10-25 03:29:23 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+connetion_string = 'mongodb+srv://test:sparta@cluster0.zm8eqgi.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
+client = MongoClient(connetion_string)
+db = client.dbsparta
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({},{'_id':False}))
+    return jsonify({'articles': articles})
+    # sample_receive = request.args.get('sample_give')
+    # print(sample_receive)
+    # return jsonify({'msg': 'GET request complete!'})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    # sample_receive = request.form['sample_give']
+    # print(sample_receive)
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
+
+    time = today.strftime('%Y.%m.%d')
+
+    doc = {
+        'file': filename,
+        'profile': profilename,
+        'title':title_receive,
+        'content':content_receive,
+        'time': time,
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'msg':'Upload complete!'})
+    
+    # return jsonify({'msg': 'POST request complete!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
---
+++ test.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+today = datetime.now()
+
+date_time = today.strftime("%Y-%m-%d-%H-%M-%S")
+print(date_time)
+
+# first_name = 'Renna'
+# last_name = 'Aprilia'
+# full_name = f'{first_name} {last_name}'
+# print(full_name)
